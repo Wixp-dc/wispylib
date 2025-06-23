126d829d40ee4d1c6011ba838a9de6870ed3c05f
Wixp <wisxpeet@gmail.com>
2025-06-23 07:39:29 +0000

    Update core.py and bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='wispylib',
     version='0.2.0',
     description='A link scraping and redirect-checking utility',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Ryu saiki',
-    author_email='you@none.com',
+    author_email='your.email@example.com',
     packages=find_packages(),
     install_requires=[
         'requests',
         'beautifulsoup4',
+        'tldextract',
     ],
     entry_points={
         'console_scripts': [
